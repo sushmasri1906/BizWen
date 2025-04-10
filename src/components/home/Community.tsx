798fc3b2c682fb9085ee02ee1f10b92d4c2037c6
sushmasri1906 <sushmasri1906@gmail.com>
2025-04-10 17:01:00 +0530

    home updated

--- src/components/home/Community.tsx
+++ src/components/home/Community.tsx
@@ -6,8 +6,11 @@ const Community = () => {
 	return (
 		<section className="bg-white text-gray-900 h-[60vh] flex items-center justify-center py-16 px-4">
 			<div className="max-w-6xl mx-auto text-center">
-				<h2 className="text-3xl font-bold mb-4">
+				<h2 className="text-3xl font-bold mb-2">
 					Connect With Like-Minded People in the WEN Community!
+				</h2>
+				<h2 className="text-2xl text-red-600 font-semibold tracking-wider mb-4">
+					Connect | Collaborate | Grow
 				</h2>
 				<p className="text-lg mb-8">
 					Join a dynamic network of ambitious professionals, entrepreneurs,
@@ -16,7 +19,7 @@ const Community = () => {
 				</p>
 				<Link
 					href="#join"
-					className=" text-red-600 transition duration-300 inline-flex items-center">
+					className="text-red-600 transition duration-300 inline-flex items-center font-medium text-lg hover:underline">
 					Join the WEN Community
 					<FaArrowRight className="ml-2" />
 				</Link>
