ed190c67649bb6888431efbb4d64f9f5ce515b3e
sushmasri1906 <sushmasri1906@gmail.com>
2025-04-16 15:30:29 +0530

    header updated

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -6,24 +6,28 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const Header = () => {
 	return (
-		<section className="relative pt-[95px] w-full h-[75vh] bg-red-600 text-white flex items-center justify-center">
+		<section className="relative pt-[95px] w-full h-[75vh] bg-gradient-to-r from-red-600 to-red-800 text-black backdrop-blur flex items-center justify-center">
 			<div className="text-center px-4 max-w-3xl">
-				<h1 className="text-4xl md:text-6xl font-bold mb-4">
-					WEN – Women Entrepreneur Network
-				</h1>
-				<p className="text-lg md:text-xl mb-6">Connect | Collaborate | Grow</p>
+				<p className="text-6xl md:text-8xl font-bold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-white to-red-300 animate-text">
+					Connect Collaborate Grow
+				</p>
 				<div className="flex justify-center gap-4 flex-wrap">
-					<button className="bg-white text-red-600 px-6 py-2 rounded-full font-semibold hover:bg-red-100 transition">
+					<button className="bg-white text-red-600 px-6 py-2 rounded-full font-semibold hover:bg-red-100 transition ease-in-out duration-300">
 						Join the Network
 					</button>
+
 					<Link
 						href="https://chat.whatsapp.com/LTnBs4Y2Ge4Hsh0Pt6UitJ"
 						target="_blank"
 						rel="noopener noreferrer"
-						className="flex items-center justify-center gap-2 bg-green-500 hover:bg-green-600 text-white px-5 py-2 rounded-full font-semibold transition">
+						className="flex items-center justify-center gap-2 bg-green-500 hover:bg-green-600 text-white px-5 py-2 rounded-full font-semibold transition ease-in-out duration-300">
 						<FaWhatsapp className="text-lg" />
-						Join WhatsApp Group
+						Free WhatsApp Open Group
 					</Link>
+
+					<button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-full font-semibold transition ease-in-out duration-300">
+						Next Meetings
+					</button>
 				</div>
 			</div>
 		</section>
