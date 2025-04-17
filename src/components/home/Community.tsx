332b2df5122943bf1a8de987afcb04ad86b1116b
sushmasri1906 <sushmasri1906@gmail.com>
2025-04-17 16:48:10 +0530

    components updated

--- src/components/home/Community.tsx
+++ src/components/home/Community.tsx
@@ -7,17 +7,17 @@ const Community = () => {
 	return (
 		<section className="relative bg-gradient-to-br from-white via-gray-50 to-red-50 text-black py-10 px-6 md:px-12 lg:px-24">
 			<div className="max-w-5xl mx-auto text-center z-10 relative">
-				<div className="mb-2">
+				<div className="">
 					<Image
 						src="https://res.cloudinary.com/dsq4uyqbb/image/upload/v1744801279/Untitled_Your_Story_epzbc9.png"
 						alt="WEN Logo"
-						width={250}
-						height={250}
+						width={400}
+						height={400}
 						className="mx-auto object-contain"
 					/>
 				</div>
 
-				<h2 className="text-3xl md:text-4xl font-extrabold text-red-600 tracking-tight mb-4">
+				<h2 className="text-xl md:text-2xl font-extrabold text-red-600 tracking-tight mb-4">
 					Connect. Collaborate. Grow.
 				</h2>
 				<p className="text-lg md:text-xl text-black leading-relaxed max-w-3xl mx-auto mb-8">
