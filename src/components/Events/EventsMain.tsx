2baf556d0837ae0d6687e1b5276740d1e109b7e9
sushmasri1906 <sushmasri1906@gmail.com>
2025-04-21 15:23:40 +0530

    events share option updated

--- src/components/Events/EventsMain.tsx
+++ src/components/Events/EventsMain.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaArrowRight, FaShareAlt } from "react-icons/fa";
-import { motion } from "framer-motion";
+
 import { Key } from "react";
 
 type Event = {
